09fff8799f558e486603bf37eb5f24fe3fbdb9b2
shatanyumi <45205300+shatanyumi@users.noreply.github.com>
2020-12-15 17:36:50 +0800

    synconize the change

--- GUI/VisualizeData.py
+++ GUI/VisualizeData.py
@@ -7,35 +7,34 @@ from PyQt5.QtWidgets import QFrame, QHBoxLayout, QApplication, QWidget
 import pyqtgraph as pg
 import qdarkstyle
 
+from ToolClasses import PyechartsGenerator
+
+
 class VisualizeData(QWidget):
     def __init__(self):
         super().__init__()
         pg.setConfigOption('background', '#19232D')
         pg.setConfigOption('foreground', 'd')
         pg.setConfigOptions(antialias=True)
-        self.initUI()
-        self.mainLayout()
+
         self.setStyleSheet(qdarkstyle.load_stylesheet_pyqt5())
-
-
-    def initUI(self):
-        self.setWindowTitle("中国各省人口")
-
-    def mainLayout(self):
-        PyechartsGenerator.PieChart().GetPie()
         self.mainhboxLayout = QHBoxLayout(self)
         self.frame = QFrame(self)
         self.mainhboxLayout.addWidget(self.frame)
         self.hboxLayout = QHBoxLayout(self.frame)
 
         self.myHtml = QWebEngineView()
+
+        self.setWindowOpacity(0.9)  # 设置窗口透明度
+
+    def mainLayout(self):
+        PyechartsGenerator.PieChart().GetPie()
         file = os.path.abspath('system.html')
         file = file.replace("\\", '/')
         print('file:///' + file)
         self.myHtml.load(QUrl('file:///' + file))
         self.hboxLayout.addWidget(self.myHtml)
         self.setLayout(self.mainhboxLayout)
-        self.setWindowOpacity(0.9)  # 设置窗口透明度
 
 
 if __name__ == '__main__':
